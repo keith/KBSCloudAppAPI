0f9644be434740e5eee147454ba2b11ca542f087
Keith Smiley <keithbsmiley@gmail.com>
2013-04-24 18:01:34 -0400

    Link vendor libraries

---
+++ Example/ExampleTests/KBSCommon.h
@@ -0,0 +1,11 @@
+//
+//  KBSCommon.h
+//  Example
+//
+//  Created by Keith Smiley on 4/24/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import "Expecta.h"
