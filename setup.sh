f3df7463e0bfe552aab333004032ae862c8d13fb
Keith Smiley <keithbsmiley@gmail.com>
2013-04-24 17:02:44 -0400

    Check for rake

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ! which rake &> /dev/null; then
+    echo "You must have rake installed to setup the test frameworks"
+    exit
+fi
+
 git submodule update --init --recursive
 echo "Setting up test frameworks..."
 cd Example/Vendor/Specta; rake > /dev/null
