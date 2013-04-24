113b460396123f36f08dcd3bb83057fb72d90633
Keith Smiley <keithbsmiley@gmail.com>
2013-04-24 17:03:51 -0400

    Add API source

---
+++ KBSCloudAppAPI.h
@@ -0,0 +1,12 @@
+//
+//  KBSCloudAppAPI.h
+//
+//  Created by Keith Smiley on 4/24/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KBSCloudAppAPI : NSObject
+
+@end
---
+++ KBSCloudAppAPI.m
@@ -0,0 +1,12 @@
+//
+//  KBSCloudAppAPI.m
+//
+//  Created by Keith Smiley on 4/24/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "KBSCloudAppAPI.h"
+
+@implementation KBSCloudAppAPI
+
+@end
