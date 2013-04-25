a1adbc5072b08b8895454e9dad9cfc30c1660fef
Keith Smiley <keithbsmiley@gmail.com>
2013-04-24 23:03:48 -0400

    Base example methods

--- Example/Example/KBSAppDelegate.m
+++ Example/Example/KBSAppDelegate.m
@@ -7,12 +7,23 @@
 //
 
 #import "KBSAppDelegate.h"
+#import "KBSCloudAppAPI.h"
 
 @implementation KBSAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-  // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  KBSCloudAppAPI *api = [KBSCloudAppAPI sharedClient];
+  [api setUsername:@"username" andPassword:@"password"];
+  NSURL *url = [NSURL URLWithString:@"http://github.com"];
+  [api shortenURL:url withName:@"Github" andBlock:^(NSString *response, NSError *error) {
+    if (error) {
+      NSLog(@"%@", error);
+    } else {
+      NSLog(@"%@", [response class]);
+      NSData *JSONData = [response dataUsingEncoding:NSUTF8StringEncoding];
+      NSLog(@"%@", [NSJSONSerialization JSONObjectWithData:JSONData options:0 error:nil]);
+    }
+  }];
 }
 
 @end
