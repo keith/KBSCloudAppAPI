c0df7ad01a71ccf3a701b1a7d154e2a85ca88a48
Keith Smiley <keithbsmiley@gmail.com>
2013-05-02 15:18:07 -0400

    Tests for shorten URL

--- Example/ExampleTests/KBSShortenURLTests.m
+++ Example/ExampleTests/KBSShortenURLTests.m
@@ -19,7 +19,7 @@ describe(@"shortenURL", ^{
       [client shortenURL:nil withName:@"foo" andBlock:^(NSURL *shortURL, NSDictionary *response, NSError *error) {}];
       expect(true).to.equal(false); // Should never reach this because of exception
     } @catch (NSException *exception) {
-      expect(exception).toNot.equal(nil);
+      expect(exception).notTo.equal(nil);
     }
   });
   
@@ -28,8 +28,70 @@ describe(@"shortenURL", ^{
       [client shortenURL:url withName:@"foo" andBlock:(void*)0];
       expect(true).to.equal(false); // Should never reach this because of exception
     } @catch (NSException *exception) {
-      expect(exception).toNot.equal(nil);
+      expect(exception).notTo.equal(nil);
     }
+  });
+  
+  it(@"should not raise an exception if no title is passed", ^{
+    @try {
+      [client shortenURL:url withName:nil andBlock:^(NSURL *shortURL, NSDictionary *response, NSError *error) {}];
+      expect(true).to.equal(true);
+    } @catch (NSException *exception) {
+      expect(exception).to.equal(nil);
+    }
+  });
+  
+  it(@"should return an error if there is no username/password", ^{
+    [client shortenURL:url withName:nil andBlock:^(NSURL *shortURL, NSDictionary *response, NSError *error) {
+      expect(shortURL).to.equal(nil);
+      expect(response).to.equal(nil);
+      expect(error).notTo.equal(nil);
+
+      NSDictionary *userInfo = [error userInfo];
+      NSString *title = [userInfo valueForKey:NSLocalizedDescriptionKey];
+      expect([title rangeOfString:@"CloudApp"].location == NSNotFound).to.equal(false); // It should contain 'CloudApp'
+
+      NSString *description  = [userInfo valueForKey:NSLocalizedRecoverySuggestionErrorKey];
+      expect([description rangeOfString:@"Missing"].location == NSNotFound).to.equal(false); // It should contain 'Missing'
+    }];
+  });
+  
+  describe(@"invalid credentials", ^{
+    it(@"should return an error from the server", ^AsyncBlock {
+      [client setUsername:@"foo" andPassword:@"bar"];
+      [client shortenURL:url withName:nil andBlock:^(NSURL *shortURL, NSDictionary *response, NSError *error) {
+        NSLog(@"Running Async Failure Examples");
+        expect(shortURL).to.equal(nil);
+        expect(response).to.equal(nil);
+        expect(error).notTo.equal(nil);
+        
+        NSDictionary *userInfo = [error userInfo];
+        NSString *title = [userInfo valueForKey:NSLocalizedDescriptionKey];
+        expect([title rangeOfString:@"CloudApp"].location == NSNotFound).to.equal(false); // It should contain 'CloudApp'
+        
+        NSString *description  = [userInfo valueForKey:NSLocalizedRecoverySuggestionErrorKey];
+        expect([description rangeOfString:@"Invalid"].location == NSNotFound).to.equal(false); // It should contain 'Invalid'
+
+        done();
+      }];
+    });
+  });
+  
+  describe(@"valid credentials", ^{
+    it(@"should return a valid response", ^AsyncBlock {
+      [client setUsername:@"" andPassword:@""];
+      [client shortenURL:url withName:nil andBlock:^(NSURL *shortURL, NSDictionary *response, NSError *error) {
+        NSLog(@"Running Async Valid Examples");
+        expect(shortURL).notTo.equal(nil);
+        expect(response).notTo.equal(nil);
+        expect(error).will.equal(nil);
+        
+        NSString *redirectURL = [response valueForKey:@"redirect_url"];
+        expect(redirectURL).will.equal([url absoluteString]);
+
+        done();
+      }];
+    });
   });
 });
 
