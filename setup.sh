d9d250acaa188de09e7b3d11bd869c7f5c228084
Keith Smiley <keithbsmiley@gmail.com>
2013-05-08 15:48:15 -0400

    Remove AFNetworking for setup script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,5 @@ git submodule update --init --recursive
 echo "Setting up test frameworks..."
 cd Example/Vendor/Specta; rake > /dev/null
 cd ../Expecta; rake > /dev/null
-echo "Checking out AFNetworking 1.2.1..."
-cd ../AFNetworking; git checkout 1.2.1 > /dev/null
 echo "Done"
 cd ../../../
