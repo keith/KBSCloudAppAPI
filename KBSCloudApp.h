a6627311c4b6bccac670c10058320106b2db1669
Keith Smiley <keithbsmiley@gmail.com>
2013-05-08 12:19:05 -0400

    Add define for duplication

--- KBSCloudApp.h
+++ KBSCloudApp.h
@@ -5,6 +5,9 @@
 //  Copyright (c) 2013 Keith Smiley. All rights reserved.
 //
 
+#ifndef _KBSCLOUDAPP_
+#define _KBSCLOUDAPP_
+
 #import "KBSCloudAppUser.h"
 #import "KBSCloudAppAPI.h"
 
@@ -16,6 +19,8 @@ NS_ENUM(NSInteger, KBSCloudAppAPIErrorCode) {
 
 static NSString * const KBSCloudAppAPIErrorDomain = @"com.keithsmiley.cloudappapi";
 static NSString * const baseAPI = @"http://my.cl.ly/";
+static NSString * const baseShortURL = @"cl.ly"
 static NSString * const itemsPath   = @"items";
 static NSString * const accountPath = @"account";
 
+#endif
