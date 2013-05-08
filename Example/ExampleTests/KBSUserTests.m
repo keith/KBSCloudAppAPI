5cc8913c830074865ed2a6de658a1f3d43ab2bf8
Keith Smiley <keithbsmiley@gmail.com>
2013-05-08 13:06:57 -0400

    Add tests for user

---
+++ Example/ExampleTests/KBSUserTests.m
@@ -0,0 +1,15 @@
+//
+//  KBSUserTests.m
+//  Example
+//
+//  Created by Keith Smiley on 5/8/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "KBSCommon.h"
+
+SpecBegin(KBSCloudAppUser)
+
+
+
+SpecEnd
