db9870621d4f6881ffda676aea1841f1a0e58a0d
Keith Smiley <keithbsmiley@gmail.com>
2013-05-08 14:14:46 -0400

    Tests for cookies and fake custom domain

--- Example/ExampleTests/KBSUserTests.m
+++ Example/ExampleTests/KBSUserTests.m
@@ -15,6 +15,10 @@ NSArray *parts = [token componentsSeparatedByString:@":"];
 __block NSString *username = [parts objectAtIndex:0];
 __block NSString *password = [parts objectAtIndex:1];
 
+beforeEach(^{
+  [KBSCloudAppUser clearCloudAppUsers];
+});
+
 describe(@"user creation", ^{
   describe(@"instance method", ^{
     it(@"should return a user with the correct parameters", ^{
@@ -104,7 +108,7 @@ describe(@"hasCustomDomain/customDomain/shortURLBase", ^{
     }];
   }); 
 
-  it(@"should return false/nil for users without custom domains", ^AsyncBlock {
+  it(@"should return false/nil/default for users without custom domains", ^AsyncBlock {
     KBSCloudAppUser *aUser = [KBSCloudAppUser userWithUsername:username andPassword:password];
     [aUser isValid:^(BOOL valid, NSError *error) {
       expect(valid).to.equal(true);
@@ -113,6 +117,39 @@ describe(@"hasCustomDomain/customDomain/shortURLBase", ^{
       expect([aUser hasCustomDomain]).to.equal(false);
       expect([aUser customDomain]).to.equal(nil);
       expect([aUser shortURLBase]).to.equal(@"cl.ly");
+
+      done();
+    }];
+  });
+
+  it(@"should return true/url/url for users with custom domains", ^AsyncBlock {
+    KBSCloudAppUser *aUser = [KBSCloudAppUser userWithUsername:@"foo" andPassword:@"bar"];
+    [aUser isValid:^(BOOL valid, NSError *error) {
+      expect(valid).to.equal(false);
+      expect(error).notTo.equal(nil);
+
+      [aUser setCustomDomain:@"foobar.com"]; // Set the URL for testing
+      expect([aUser hasCustomDomain]).to.equal(true);
+      expect([aUser customDomain]).to.equal(@"foobar.com");
+      expect([aUser shortURLBase]).to.equal(@"foobar.com");
+
+      done();
+    }];
+  });
+});
+
+describe(@"clear cookies", ^{
+  it(@"should actually clear all the cookies", ^AsyncBlock {
+    KBSCloudAppUser *aUser = [KBSCloudAppUser userWithUsername:username andPassword:password];
+    [aUser isValid:^(BOOL valid, NSError *error) {
+      expect(valid).to.equal(true);
+      expect(error).to.equal(nil);
+
+      NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:[NSURL URLWithString:baseAPI]];
+      expect(cookies.count).to.beGreaterThanOrEqualTo(1);
+      [KBSCloudAppUser clearCloudAppUsers];
+      cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookiesForURL:[NSURL URLWithString:baseAPI]];
+      expect(cookies.count).to.equal(0);
 
       done();
     }];
