77373e2b694c6077ae44bd93bde8432d73b1d302
Keith Smiley <keithbsmiley@gmail.com>
2013-05-09 13:19:51 -0400

    Add KBSCloudAppFile

---
+++ KBSCloudAppFile.h
@@ -0,0 +1,12 @@
+//
+//  KBSCloudAppFile.h
+//
+//  Created by Keith Smiley
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KBSCloudAppFile : NSObject
+
+@end
---
+++ KBSCloudAppFile.m
@@ -0,0 +1,12 @@
+//
+//  KBSCloudAppFile.m
+//
+//  Created by Keith Smiley
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "KBSCloudAppFile.h"
+
+@implementation KBSCloudAppFile
+
+@end
